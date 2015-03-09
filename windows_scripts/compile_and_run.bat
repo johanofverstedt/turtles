bb63cd50530175b6ecf195cdf6e48dabbc0879d6
Johan Öfverstedt <johan_over@hotmail.com>
2015-03-09 23:45:12 +0100

    Fixed doc structure.

--- windows_scripts/compile_and_run.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-javac World.java Turtle.java SampleProgram.java
-java SampleProgram
-cd windows_scripts
-
-rem pause
--- windows_scripts/make_javadoc.bat
+++
@@ -1,4 +0,0 @@
-mkdir ..\doc
-javadoc -d ..\doc ..\Turtle.java ..\World.java -nodeprecatedlist -nohelp -notree -noindex -windowtitle "Turtle and World Documentation"
-
-pause
