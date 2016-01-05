fd290f45ac1483b6e7f845312d936dbb9fa9fa97
Johan Öfverstedt <johan.ofverstedt@gmail.com>
2016-01-05 15:24:37 +0100

    Removed international characters from comments.

--- Turtle.java
+++ Turtle.java
@@ -1,6 +1,6 @@
 
 /**
- * Copyright (c) 2015, Johan Öfverstedt <johan.ofverstedt@gmail.com>
+ * Copyright (c) 2015, Johan Ofverstedt <johan.ofverstedt@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
  * provided that the above copyright notice and this permission notice appear in all copies.
